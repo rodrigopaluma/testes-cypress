4299766d6276d44847ee704442b319504829d6b6
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-06-03 08:04:25 -0300

    Utilização dos métodos pause e debug do Cypress.

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,11 +1,13 @@
 /// <reference types ="cypress" />
 
 describe('Cypress Basic', ()=>{
-    it('Should visit a page and assert title', ()=>{
+    it.only('Should visit a page and assert title', ()=>{
         cy.visit('https://wcaquino.me/cypress/componentes.html');
+
+        cy.pause();
         
         cy.title().should('be.equal','Campo de Treinamento');
-        cy.title().should('contain','Campo');
+        cy.title().should('contain','Campo').debug();
 
         cy.title()
             .should('be.equal','Campo de Treinamento')
@@ -13,5 +15,12 @@ describe('Cypress Basic', ()=>{
 
         //TODO imprimir o title no console
         //TODO escrever o title em um campo de texto
-    })
+    });
+
+    it('Should find and interact with an element', ()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value','Obrigado!');
+    });
 })
