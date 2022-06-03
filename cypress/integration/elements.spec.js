6ee78ca8c6e35bd641549753f0c70c66c49d13e7
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-06-03 08:05:31 -0300

    Interagindo com elementos: Textos e Links.

---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,21 @@
+/// <reference types ="cypress" />
+
+describe('Work with basic elements', ()=>{
+    it('Texts', ()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+        cy.get('body').should('contain','Cuidado');
+        cy.get('span').should('contain','Cuidado');
+        cy.get('.facilAchar').should('contain','Cuidado');
+        cy.get('.facilAchar').should('have.text','Cuidado onde clica, muitas armadilhas...');
+    });
+    it.only('Links', ()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+        cy.get('[href="#"]').click();
+        cy.get('#resultado').should('have.text','Voltou!');
+        
+        cy.reload();
+        cy.get('#resultado').should('have.not.text','Voltou!');
+        cy.contains('Voltar').click();
+        cy.get('#resultado').should('have.text','Voltou!');
+    })
+})
