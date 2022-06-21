72432b8c2cd0bf5ba0d448def3777362c2975d38
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-06-21 18:10:13 -0300

    Trabalhando com each().

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -23,5 +23,26 @@ describe('Dinamic tests', () => {
             cy.get('#formCadastrar').click()
             cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
         });
-    })
+    });
+
+    it.only('Deve selecionar todos usando o each', () => {
+        cy.get('#formNome').type('Rodrigo',{delay:150});
+        cy.get('#formSobrenome').type('Paluma',{delay:150});
+        cy.get(`[name=formSexo][value=M]`).click()
+
+        cy.get(`[name=formComidaFavorita]`).each($el => {
+            //$el.click();
+            if($el.val() === 'Vegetariano'){
+                cy.wrap($el).click();
+            }
+        })
+
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+
+        //cy.clickAlert('#formCadastrar','Tem certeza que voce eh vegetariano?')
+    });
 });
