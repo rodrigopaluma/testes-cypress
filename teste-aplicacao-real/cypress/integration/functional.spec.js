4d126da0560b04450fc50141c7a0d27e5ef46d58
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-06-23 08:45:18 -0300

    Criação do teste de Edição de Conta Bancária.

--- teste-aplicacao-real/cypress/integration/functional.spec.js
+++ teste-aplicacao-real/cypress/integration/functional.spec.js
@@ -1,15 +1,14 @@
 /// <reference types ="cypress" />
 
-describe('Teste em nível funcional', () => {
-    /* before(()=>{
-        cy.visit('https://barrigareact.wcaquino.me/');
-        cy.get('[data-test=email]').type('rodrigo.paluma@gmail.com', {delay:100});
-        cy.get('[data-test=passwd]').type('CypressNow', {delay:100});
-        cy.get('.btn').click();
-        cy.get('.toast-message').should('exist');
-    }); */
+import loc from './../support/locators'
 
-    it('Login', function() {
+describe('Teste em nível funcional', () => {
+    before(()=>{
+        cy.login()
+        cy.clearData()
+    });
+
+    /* it('Login', function() {
         cy.visit('https://barrigareact.wcaquino.me/');
         cy.fixture('userData').as('usuario').then(()=>{
             cy.get('[data-test=email]').type(this.usuario.email, {delay:100});
@@ -17,5 +16,25 @@ describe('Teste em nível funcional', () => {
             cy.get('.btn').click();
             cy.get('.toast-message').should('contain', 'Bem vindo');
         })
+    }); */
+
+    it('Cadastro de Conta', () => {
+        cy.get(loc.MENU.SETTINGS).click();
+        cy.get(loc.MENU.CONTAS).click();
+        cy.get(loc.CONTAS.NOME).type('Conta Corrente Bradesco', {delay:50});
+        cy.get(loc.CONTAS.BTN_SALVAR).click();
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso');
+    });
+
+    it('Edição de Conta', () => {
+        //cy.clearData();
+        cy.get(loc.MENU.SETTINGS).click();
+        cy.get(loc.MENU.CONTAS).click();
+        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click();
+        cy.get(loc.CONTAS.NOME)
+            .clear()
+            .type('Conta Corrente Bradesco - Editado', {delay:50});
+        cy.get(loc.CONTAS.BTN_SALVAR).click();
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso');
     });
 });
