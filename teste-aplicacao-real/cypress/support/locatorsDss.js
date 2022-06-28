bbba4c136040777fd0b211998ca3d9e957cbf02b
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-06-28 08:48:20 -0300

    Locators da aplicação Angular utilizada em teste.

---
+++ teste-aplicacao-real/cypress/support/locatorsDss.js
@@ -0,0 +1,14 @@
+const locatorsDss = {
+    LOGIN: {
+        USERNAME: '#mat-input-0',
+        PASSWORD: '#mat-input-1',
+        DIVISION: '.mat-form-field-type-mat-select > .mat-form-field-wrapper > .mat-form-field-flex',
+        DIREITA: '#mat-option-0 > .mat-option-text',
+        BTN_LOGIN: 'form.ng-dirty > .btn-entrar',
+    },
+    MENU: {
+        SETTINGS: '//button[2]',
+    }
+}
+
+export default locatorsDss;
